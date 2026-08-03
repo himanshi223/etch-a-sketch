e5e1f992a04ef321c4b36809dad5d13dd1f2f35d
Himanshi <158239904+himanshi223@users.noreply.github.com>
2026-08-03 20:05:54 +0530

    add eventlistener to draw

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ for(let i=0;i<16*16;i++){
     let div = document.createElement("div");
     container.appendChild(div);
 }
+
+//the mechanism to draw
+
+container.addEventListener("mouseover",(e)=>{
+    e.target.style.backgroundColor = "blue";
+})
+
