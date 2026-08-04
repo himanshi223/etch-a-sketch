92454e836f50df7224b797a983b545ed80a36b56
Himanshi <158239904+himanshi223@users.noreply.github.com>
2026-08-04 09:00:46 +0530

    add button to change grid dimensions
    added a button that asks for the number of squares per side and changes the grid accordingly

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 let container = document.querySelector(".grid-container");
+let changeButton = document.querySelector(".change-grid");
 
-for(let i=0;i<16*16;i++){
-    let div = document.createElement("div");
-    container.appendChild(div);
+function createGrid(side){
+    if(side>100)
+        return;
+    for(i=0;i<side*side;i++){
+        const div = document.createElement("div");
+        container.appendChild(div);
+    }
 }
+
+createGrid(16);
 
 //the mechanism to draw
 
 container.addEventListener("mouseover",(e)=>{
-    e.target.style.backgroundColor = "blue";
+    e.target.classList.add("hovered");
 })
+
+
+changeButton.addEventListener("click",(e)=>{
+    const squares = prompt("Number of squares per side: ",16);
+    createGrid(squares);
+})
+
 
